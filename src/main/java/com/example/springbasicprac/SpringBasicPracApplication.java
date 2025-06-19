b9a995dc5ffe3ee42c273185ddfc37bcfd09fe93
leegaarden <dlwjddnjs081723@naver.com>
2025-06-19 13:55:57 +0900

    init

---
+++ src/main/java/com/example/springbasicprac/SpringBasicPracApplication.java
@@ -0,0 +1,13 @@
+package com.example.springbasicprac;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBasicPracApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(SpringBasicPracApplication.class, args);
+  }
+
+}
---
+++ src/test/java/com/example/springbasicprac/SpringBasicPracApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.springbasicprac;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBasicPracApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
